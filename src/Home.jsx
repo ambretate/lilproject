f6762bd0ffb0849bb52d6be2957f430485dd2150
Ambre Tate <ambretate@gmail.com>
2024-02-25 22:04:49 -0500

    added alt tag to Home.jsx component image

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css"
 
 function Home() {
   return (
-    <img src="Images/Chasing_Daylight_Landing_page.jpeg" className='landing'/>
+    <img src="Images/Chasing_Daylight_Landing_page.jpeg" className='landing' alt="Chasing Daylight logo" />
   )
 }
 
